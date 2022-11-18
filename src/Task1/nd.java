ac6b26aab1df37697dc9f9acb7d833208b6e0dc5
Dmitriy <demonmr02@gmail.com>
2022-11-18 12:33:41 +0200

    FirstTask

--- src/Task1/nd.java
+++ src/Task1/nd.java
@@ -2,18 +2,16 @@ package Task1;
 
 public class nd {
     public static void main(String[] args) {
-       double salary = 1700;
-       if (!(salary > 0)){
-           System.out.println("начение должно быть больше нуля");
-       }else {
-        if (salary > 500 && salary < 1500){
-            System.out.println("You JUNIOR");
-            }else if (salary > 1500  && salary< 2500){
-            System.out.println("You MIDLE");
+        double salary = 1700;
+        if (!(salary > 0)) {
+            System.out.println("начение должно быть больше нуля");
+        } else {
+            if (salary > 500 && salary < 1500) {
+                System.out.println("You JUNIOR");
+            } else if (salary > 1500 && salary < 2500) {
+                System.out.println("You MIDDLE");
+            }
         }
-       }
-
-
 
 
     }
